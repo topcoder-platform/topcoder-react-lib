f1a12b45dbfa47c9c6f9873135965bbd592c530c
Thomas Kranitsas <kranitsasthomas@gmail.com>
2020-01-28 16:45:35 +0200

    API upgrades for the payment tool

--- config/test.js
+++ config/test.js
@@ -2,6 +2,8 @@ module.exports = {
   API: {
     V2: 'https://api.topcoder-dev.com/v2',
     V3: 'https://api.topcoder-dev.com/v3',
+    V4: 'https://api.topcoder-dev.com/v4',
+    V5: 'https://api.topcoder-dev.com/v5',
   },
   dummyConfigKey: 'Dummy config value',
 };
