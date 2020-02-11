a64329b888809d6f59d904e23f3240bfb00469f3
Sumit Daga <sumit.daga66@gmail.com>
2020-02-11 16:56:32 +0530

    fix tests

--- config/test.js
+++ config/test.js
@@ -2,6 +2,7 @@ module.exports = {
   API: {
     V2: 'https://api.topcoder-dev.com/v2',
     V3: 'https://api.topcoder-dev.com/v3',
+    V5: 'https://api.topcoder-dev.com/v5',
   },
   dummyConfigKey: 'Dummy config value',
 };
