023db8bc93f2a5147638b838b3dc325b9ba7b930
Luiz Ricardo Rodrigues <contato@luizrrodrigues.com.br>
2020-08-31 19:50:47 -0300

    Merge pull request #238 from topcoder-platform/issue-4739-hotfix
    Fix Review Opportunities QA filter

--- src/services/reviewOpportunities.js
+++ src/services/reviewOpportunities.js
@@ -10,12 +10,33 @@ import { getApi } from './api';
  * Sync the fields of V3 and V5 for front-end to process successfully
  * @param challenges - challenges to normalize
  */
+<<<<<<< HEAD
 export function normalizeChallenges(challenges) {
   if (challenges) {
     _.map(challenges, (ch) => {
       const { challenge } = ch;
       if (challenge.technologies && challenge.technologies.includes('Data Science')) {
         challenge.track = 'DATA_SCIENCE';
+=======
+export function normalizeChallenges(opportunities) {
+  if (opportunities) {
+    /* Issue#4739 : Temporary add track to review opportunities challenges
+     * until receive API V5 update. */
+    _.map(opportunities, (opportunity) => {
+      const { challenge } = opportunity;
+      challenge.track = COMPETITION_TRACKS.DEVELOP;
+      if (challenge.technologies) {
+        if (challenge.technologies.includes(COMPETITION_TRACKS.DATA_SCIENCE)) {
+          challenge.track = COMPETITION_TRACKS.DATA_SCIENCE;
+        }
+      } else if (challenge.subTrack === OLD_SUBTRACKS.TEST_SUITES
+          || challenge.subTrack === OLD_SUBTRACKS.BUG_HUNT
+          || challenge.subTrack === OLD_COMPETITION_TRACKS.TEST_SCENARIOS
+          || challenge.subTrack === OLD_COMPETITION_TRACKS.TESTING_COMPETITION) {
+        challenge.track = COMPETITION_TRACKS.QA;
+      } else if (challenge.track === OLD_COMPETITION_TRACKS.DESIGN) {
+        challenge.track = COMPETITION_TRACKS.DESIGN;
+>>>>>>> 978da83... Merge pull request #238 from topcoder-platform/issue-4739-hotfix
       }
       return _.defaults(ch, { challenge });
     });
--- src/utils/tc.js
+++ src/utils/tc.js
@@ -17,6 +17,20 @@ export const COMPETITION_TRACKS = {
   QA: 'Quality Assurance',
 };
 
+export const OLD_COMPETITION_TRACKS = {
+  DATA_SCIENCE: 'DATA_SCIENCE',
+  DESIGN: 'DESIGN',
+  DEVELOP: 'DEVELOP',
+  QA: 'QA',
+};
+
+export const OLD_SUBTRACKS = {
+  TEST_SUITES: 'TEST_SUITES',
+  BUG_HUNT: 'BUG_HUNT',
+  TEST_SCENARIOS: 'TEST_SCENARIOS',
+  TESTING_COMPETITION: 'TESTING_COMPETITION',
+};
+
 /**
  * Review Opportunity types
  */
